03c774d469cb2aae356a5f53efbc56b2b7769f5f
Osama Ayman Ibrahim <50743464+osamaayman1005@users.noreply.github.com>
2026-04-17 15:29:04 +0200

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-RK_Jn1XY.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/work/Projects/elegant-union-invites/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/elegant-union-invites/assets/index-CfQAoExO.js"] }, "/": { filePath: "C:/work/Projects/elegant-union-invites/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/elegant-union-invites/assets/index-Bj3qZvg7.js"] } }, clientEntry: "/elegant-union-invites/assets/index-CfQAoExO.js" });
+export {
+  tsrStartManifest
+};
